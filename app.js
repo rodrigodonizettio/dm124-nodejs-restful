464eab23a39cc480dccb8181a9fad3764fb96441
rodrigodonizettio <rodrigodonizettio@gmail.com>
2019-08-17 12:02:54 -0300

    Add App.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    res.status(200).json({
+        message : 'It just works baby!'
+    });
+});
+
+module.exports = app;
