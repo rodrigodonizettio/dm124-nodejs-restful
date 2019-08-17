a03ef46641177f7dfd4bc71590fd92e23fd21783
rodrigodonizettio <rodrigodonizettio@gmail.com>
2019-08-17 17:14:14 -0300

    Bug fix in NotFound js

--- api/middleware/not-found.js
+++ api/middleware/not-found.js
@@ -2,7 +2,6 @@ const notFound = (req, res, next) => {
     res.status(404).json({
         error: `Route ${req.method} ${req.url} was not found!`
     });
-    next();
 };
 
 module.exports = notFound;
